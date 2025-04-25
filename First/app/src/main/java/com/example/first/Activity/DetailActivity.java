c6bd57d231c230e22cdec1bd5c21d0e11f85c5de
aXayitov <hayitovabdurahmon76@gmail.com>
2025-04-26 00:40:11 +0500

    Added some logics of backend

--- First/app/src/main/java/com/example/first/Activity/DetailActivity.java
+++ First/app/src/main/java/com/example/first/Activity/DetailActivity.java
@@ -1,4 +1,4 @@
-package com.example.first;
+package com.example.first.Activity;
 
 import android.os.Bundle;
 
@@ -8,18 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.first.R;
+
 public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_detail);
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
