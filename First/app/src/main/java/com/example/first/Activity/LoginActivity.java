1bf9a076c3e5cc3875e199a59ff53dcb493ed282
aXayitov <hayitovabdurahmon76@gmail.com>
2025-04-26 18:42:49 +0500

    Connected Intro and Login pages backend.

--- First/app/src/main/java/com/example/first/Activity/LoginActivity.java
+++ First/app/src/main/java/com/example/first/Activity/LoginActivity.java
@@ -1,6 +1,11 @@
 package com.example.first.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,16 +16,30 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.first.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+private EditText userEdt, passEdt;
+private Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        initView();
+    }
+
+    private void initView() {
+        userEdt = findViewById(R.id.editTextText);
+        passEdt = findViewById(R.id.editTextPassword);
+        loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(userEdt.getText().toString().isEmpty() || passEdt.getText().toString().isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Please fill your username and password", Toast.LENGTH_SHORT).show();
+                }
+                else if(userEdt.getText().toString().equals("Abdurakhmon") && passEdt.getText().toString().equals("mob123")){
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }
+            }
         });
     }
 }
